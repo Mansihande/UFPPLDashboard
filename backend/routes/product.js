774ab7eb05a4efbbe48da551d192be814249b669
Mansihande <mansihande09@gmail.com>
2024-06-05 17:43:13 +0530

    changes in category and added api of products (insert , update , delete, getsingle , getall, getbycategory , getspecific

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const {insertProduct}= require('../controller/product')
+const {insertProduct,updateProduct,deleteProduct,getAllProducts,getSingleProduct,getCategoryProducts,getSubcategoryProducts,getSubSubcategoryProducts}= require('../controller/product')
 const {uploadPhoto} = require('../middleware/fileUpload')
-const {insertCategory,updateCategory,updateSubCategory,updatesubsubcategory,deletecategory,deletesubcategory,deletesubsubcategory,getAll,getSpecificCategory,getSpecificSubcategory}= require('../controller/category')
+const {insertCategory,insertSubCategory,insertSubSubCategory,updateCategory,updateSubCategory,updatesubsubcategory,deletecategory,deletesubcategory,deletesubsubcategory,getAll,getSpecificCategory,getSpecificSubcategory,getSpecificSubSubcategory}= require('../controller/category')
 
 
 router.post('/insertProduct',uploadPhoto,insertProduct);
-// router.get('/getService',getService)
-// router.put('/updateService', uploadPhoto, updateService);
-// router.delete('/deleteService',deleteService);
-// router.get('/singleService',getServiceById)
+router.get('/getAllProducts',getAllProducts)
+router.put('/updateProduct', uploadPhoto, updateProduct);
+router.delete('/deleteProduct',deleteProduct);
+router.get('/getSingleProduct',getSingleProduct)
+router.get('/getCategoryProducts',getCategoryProducts)
+router.get('/getSubcategoryProducts',getSubcategoryProducts)
+router.get('/getSubSubcategoryProducts',getSubSubcategoryProducts)
+
 
 router.post('/insertCategory',insertCategory)
+router.post('/insertSubCategory',insertSubCategory)
+router.post('/insertSubSubCategory',insertSubSubCategory)
 router.put('/updateCategory',updateCategory)
 router.put('/updateSubCategory',updateSubCategory)
 router.put('/updatesubsubcategory',updatesubsubcategory)
@@ -21,4 +27,5 @@ router.delete('/deletesubsubcategory',deletesubsubcategory)
 router.get('/getAll',getAll)
 router.get('/getSpecificCategory',getSpecificCategory)
 router.get('/getSpecificSubcategory',getSpecificSubcategory)
+router.get('/getSpecificSubSubcategory',getSpecificSubSubcategory)
 module.exports = router;
